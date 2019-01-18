55ef5385ff19e8c6759716e3d1c016c880b3413a
liinapytt <liina.rebassoo@khk.ee>
2019-01-18 15:43:57 +0200

    Build a test stub

--- src/Receipt.php
+++ src/Receipt.php
@@ -1,11 +1,22 @@
 <?php
 namespace TDD;
 class Receipt {
-    public function total(array $items = []){
-        return array_sum($items);
+    // Adding coupon calculation to total
+    public function total(array $items = [], $coupon) {
+        $sum = array_sum($items);
+        if (!is_null($coupon)) {
+            return $sum - ($sum * $coupon);
+        }
+        return $sum;
     }
     //Adding a method tax that calculates the tax sum
     public function tax($amount, $tax) {
         return ($amount * $tax);
+    }
+
+    //Adding method that posts the total sum of tax
+    public function postTaxTotal($items, $tax, $coupon) {
+        $subtotal = $this->total($items, $coupon);
+        return $subtotal + $this->tax($subtotal, $tax);
     }
 }
--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -17,13 +17,38 @@ class ReceiptTest extends TestCase {
     // Refactoring testTotal
     public function testTotal() {
         $input = [0,2,5,8];
-        $output = $this->Receipt->total($input);
+        $coupon = null;
+        $output = $this->Receipt->total($input, $coupon);
         $this->assertEquals(
             15,
             $output,
             'Sum must be 15'
         );
     }
+    //Adding a function to sum the total with discount coupon
+    public function testTotalAndCoupon() {
+        $input = [0,2,5,8];
+        $coupon = 0.20;
+        $output = $this->Receipt->total($input, $coupon);
+        $this->assertEquals(
+            12,
+            $output,
+            'When summing the total should equal 12'
+        );
+    }
+    //Adding a mock method testPostTaxTotal that uses MockBuilder class
+    public function testPostTaxTotal() {
+        $Receipt = $this->getMockBuilder('TDD\Receipt')
+            ->setMethods(['tax', 'total'])
+            ->getMock();
+        $Receipt->method('total')
+            ->will($this->returnValue(10.00));
+        $Receipt->method('tax')
+            ->will($this->returnValue(1.00));
+        $result = $Receipt->postTaxTotal([1,2,5,8], 0.20, null);
+        $this->assertEquals(11.00, $result);
+    }
+
     // Adding a new method testTax according to Add-Act-Assert
     public function testTax() {
         $inputAmount = 10.00;
