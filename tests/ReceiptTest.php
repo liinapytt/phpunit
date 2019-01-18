9214b3a50cb7d144eae44937a7b91610da85a7f7
liinapytt <liina.rebassoo@khk.ee>
2019-01-18 14:49:16 +0200

    Extend a unit test

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -6,13 +6,22 @@ use PHPUnit\Framework\TestCase;
 use TDD\Receipt;
 
 class ReceiptTest extends TestCase {
+    //Adding new method setUp to provide instance of class
+    public function setUp() {
+        $this->Receipt = new Receipt();
+    }
+    //Adding new method tearDown to let out test in isolation
+    public function tearDown() {
+        unset($this->Receipt);
+    }
+    // Refactoring testTotal
     public function testTotal() {
-        $Receipt = new Receipt();
+        $input = [0,2,5,8];
+        $output = $this->Receipt->total($input);
         $this->assertEquals(
-            14,
-            $Receipt->total([0,2,5,8]),
+            15,
+            $output,
             'Sum must be 15'
         );
-
 }
 }
